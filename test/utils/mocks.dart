e9f48fb468b71a910b80ff66c34bd50a49b9a3d6
ushi <>
2026-01-01 22:51:43 +0900

    feature: providerContainer & mocks

---
+++ test/utils/mocks.dart
@@ -0,0 +1,6 @@
+import 'package:switch_app_freezed/data/repository/switch_repository.dart';
+import 'package:switch_app_freezed/domain/logic/switch_notifier.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([SwitchNotifier, SwitchRepository])
+void main() {}
