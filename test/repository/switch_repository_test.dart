0fe46c2e8961b07c496243c186d95f323949e65d
ushi <>
2026-01-04 15:33:25 +0900

    fix: switch_repository_test

--- test/repository/switch_repository_test.dart
+++ test/repository/switch_repository_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:switch_app_freezed/data/dto/switch_dto.dart';
+import 'package:switch_app_freezed/data/repository/firebase_firestore_repository.dart';
 import 'package:switch_app_freezed/data/repository/switch_repository.dart';
 
 import '../utils/create_container.dart';
@@ -9,96 +9,24 @@ import '../utils/mocks.mocks.dart';
 const String documentId = 'x6qOk5aT9ojUgoBJrEWS';
 
 void main() {
-  late MockSwitchRepository mockSwitchRepository;
-  setUp(() {
-    mockSwitchRepository = MockSwitchRepository();
-  });
-  test('Firebaseからデータを取得し、SwitchDtoに変換するできているか確認する', () async {
-    // Repository の subscribeSwitchIsEnabled() が返す Stream をモックする。
-    when(
-      mockSwitchRepository.subscribeSwitchIsEnabled(documentId: documentId),
-    ).thenAnswer((_) => Stream.value(SwitchDto(isEnabled: false)));
-    // whenについて理解したいかな。
-
-    // refの代わりにcontainerを使用する。
-    final container = createContainer(
-      overrides: [
-        switchRepositoryProvider.overrideWith((_) => mockSwitchRepository),
-      ],
-    );
-
-    // 読み込む
-    final streamSwitchDto = container
-        .read(switchRepositoryProvider)
-        .subscribeSwitchIsEnabled(documentId: documentId);
-
-    final switchDto = await streamSwitchDto.first;
-
-    // 読み込んだSwitchState（isEnabled: false）になっていることを確認する。
-    expect(switchDto, SwitchDto(isEnabled: false));
-  });
-
-  test('SwitchをOnにすると、SwitchDtoがtrueになることを確認する', () async {
-    // Repository の subscribeSwitchIsEnabled() が返す Stream をモックする。
-    when(
-      mockSwitchRepository.subscribeSwitchIsEnabled(documentId: documentId),
-    ).thenAnswer((_) => Stream.value(SwitchDto(isEnabled: false)));
+  final mockFirestore = MockFirebaseFirestore();
+  final mockCollection = MockCollectionReference<Map<String, dynamic>>();
+  final mockDocRef = MockDocumentReference<Map<String, dynamic>>();
+  final mockSnapshot = MockDocumentSnapshot<Map<String, dynamic>>();
+  test('subscribeSwitchIsEnabled が正しくデータを取得できるか', () async {
+    when(mockFirestore.collection('switch')).thenReturn(mockCollection);
+    when(mockCollection.doc(documentId)).thenReturn(mockDocRef);
+    when(mockDocRef.snapshots()).thenAnswer((_) => Stream.value(mockSnapshot));
+    when(mockSnapshot.data()).thenReturn({'isEnabled': false});
 
     final container = createContainer(
-      overrides: [
-        switchRepositoryProvider.overrideWith((_) => mockSwitchRepository),
-      ],
+      overrides: [firebaseFirestoreProvider.overrideWithValue(mockFirestore)],
     );
 
-    when(
-      mockSwitchRepository.subscribeSwitchIsEnabled(documentId: documentId),
-    ).thenAnswer((_) => Stream.value(SwitchDto(isEnabled: true)));
+    final repository = container.read(switchRepositoryProvider);
+    final stream = repository.subscribeSwitchIsEnabled(documentId: documentId);
+    final result = await stream.first;
 
-    await container
-        .read(switchRepositoryProvider)
-        .switchOn(documentId: documentId);
-
-    // 購読するstreamを取得する。
-    final streamSwitchDto = container
-        .read(switchRepositoryProvider)
-        .subscribeSwitchIsEnabled(documentId: documentId);
-
-    // 購読するstreamからデータを取得する。
-    final switchDto = await streamSwitchDto.first;
-
-    expect(switchDto, SwitchDto(isEnabled: true));
-  });
-
-  test('SwitchをOffにすると、SwitchDtoがfalseになることを確認する', () async {
-    // Repository の subscribeSwitchIsEnabled() が返す Stream をモックする。
-    when(
-      mockSwitchRepository.subscribeSwitchIsEnabled(documentId: documentId),
-    ).thenAnswer((_) => Stream.value(SwitchDto(isEnabled: true)));
-
-    // refの代わりにcontainerを使用する。
-    // SwitchRepositoryをmockに置き換えている。
-    final container = createContainer(
-      overrides: [
-        switchRepositoryProvider.overrideWith((_) => mockSwitchRepository),
-      ],
-    );
-
-    when(
-      mockSwitchRepository.subscribeSwitchIsEnabled(documentId: documentId),
-    ).thenAnswer((_) => Stream.value(SwitchDto(isEnabled: false)));
-
-    await container
-        .read(switchRepositoryProvider)
-        .switchOff(documentId: documentId);
-
-    // 購読するstreamを取得する。
-    final streamSwitchDto = container
-        .read(switchRepositoryProvider)
-        .subscribeSwitchIsEnabled(documentId: documentId);
-
-    // 購読するstreamからデータを取得する。
-    final switchDto = await streamSwitchDto.first;
-
-    expect(switchDto, SwitchDto(isEnabled: false));
+    expect(result.isEnabled, false);
   });
 }
